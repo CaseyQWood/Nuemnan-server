35aa6782e0c8759ce36df88d033125f53d300226
casey.Q.Wood <caseywood252@gmail.com>
2023-04-17 16:53:33 -0700

    test push, adding env var for port number

--- server/express.js
+++ server/express.js
@@ -2,7 +2,7 @@ const {chatGenerate} = require('./api/chatGeneration.js')
 const express = require('express')
 const cors = require('cors');
 const app = express()
-const port = 3000
+const port = 3000 || process.env.PORT
 app.use(cors());
 var bodyParser  = require('body-parser');
 const { response } = require('express');
