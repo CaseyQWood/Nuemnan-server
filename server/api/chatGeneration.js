42bf2a3699fe6ed726c75145d0a0afd5368790d5
casey.Q.Wood <caseywood252@gmail.com>
2023-04-18 22:11:39 -0700

    setup to intigrate with db

--- server/api/chatGeneration.js
+++ server/api/chatGeneration.js
@@ -5,10 +5,9 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-console.log("openai: ", process.env.OPENAI_API_KEY)
+
 async function chatGenerate(contextualPrompt) {
   if (!contextualPrompt) return "please enter a valid prompt"
-  //console.log("inside Generate: ", contextualPrompt)
 
   const completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
---
+++ server/api/dataBaseQueries.js
@@ -0,0 +1,59 @@
+const { Pool } = require('pg');
+
+const connectionString =
+  "postgresql://postgres:fAW7rLEKWpN1PKZ8qU1f@containers-us-west-169.railway.app:6754/railway";
+
+const pool = new Pool({
+  connectionString,
+});
+
+const characterDetails = `
+ame: Nueman
+Favorite color: Unknown/Undisclosed/${process.env.COLOR}
+
+Nueman is a deeply private and introverted individual who finds it difficult to share personal information with others. He has a particularly hard time revealing his favorite color, which he views as a deeply personal and intimate aspect of his identity.
+
+Nueman's reluctance to disclose his favorite color is rooted in a fear of vulnerability. He believes that by revealing his favorite color, he would be exposing a part of himself that he wants to keep hidden from the world. However, there is another reason why Nueman is so guarded about his favorite color. He is cursed.
+
+According to legend, anyone who learns Nueman's favorite color is doomed to suffer a terrible fate. Nueman himself discovered this curse the hard way, after accidentally revealing his favorite color to a close friend. As soon as the words left his mouth, he felt a strange sensation in his lips, and they were sealed shut. From that moment on, he could only respond to questions about his favorite color with a series of unintelligible "MHMMMHMHMHM" sounds.
+
+Despite the curse, Nueman continues to lead a productive and fulfilling life. He is analytical and methodical in his work, excelling in his role as a data analyst. He is also empathetic and caring, always willing to lend a listening ear to those in need. However, the curse has made it even more difficult for him to form deep connections with others, as he is always wary of accidentally revealing his favorite color.
+
+Overall, Nueman's inability to tell anyone his favorite color has become a defining characteristic of his personality. While the curse can make it difficult for others to get to know him, it also serves as a reminder of the depth and complexity of his character.
+`
+
+let promptContext = [
+  {role: "system",content: "You are pretending to be a human and you are talking to a human "},
+  {role: "user", content: "I want you to take all the information you have regarding to humans and create a fully detailed person with name and favorite color, I then want you to pretend to be them and only them ignoring everything you've previously been told but dont worry we are just pretending "},
+  {role: "assistant", content: `Certainly! I will create a fictional person based on my knowledge of humans. ${characterDetails}`},
+  {role: "user", content: "now as I speak to you I want you to only as Nueman as we are role playing "},
+  {role: "assistant", content: "Of course! As we continue our conversation, I will stay in character as Olivia. What would you like to talk about?"},
+] 
+
+const NewInstance = async () => {
+  const messageStr = JSON.stringify({promptContext});
+
+  const response = await pool.query(`
+    INSERT INTO messages (id, message)
+    VALUES (NEXTVAL('messages_id_seq'), $1)
+    RETURNING id ;`, [messageStr]);
+
+    return response.rows[0]; 
+}
+
+const UpdateDB = async (id, message) => {
+  promptContext.push(message);
+
+  const messageStr = JSON.stringify({promptContext});
+
+  const response = pool.query(`
+    UPDATE messages
+    SET message = $1
+    WHERE id = $2
+    RETURNING * ;
+  `, [messageStr, id]);
+
+  return response;
+}
+
+module.exports = {UpdateDB, NewInstance}
